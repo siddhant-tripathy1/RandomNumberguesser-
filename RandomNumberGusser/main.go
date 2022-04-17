cfb1152df0e7a7ebea8b72a1957b16dea215bf6b
siddhant tripathy <88921213+siddhant-tripathy1@users.noreply.github.com>
2022-04-17 13:01:11 +0530

    I have added

--- RandomNumberGusser/main.go
+++ RandomNumberGusser/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
 )
 
+const maxTurn = 4 // change the maxTurn if you wanna to 
+
+// this a game where you have to enter a number and in line number 16 loop will run(you can change the number of repetation if you like ) and if your guess number = random number you won
 func main() {
-	fmt.Println("Hi this is siddhant kumar tripathy")
+	rand.Seed(time.Now().UnixNano())
+	guess, _ := strconv.Atoi(os.Args[1])
+	for turn := 0; turn < maxTurn; turn++ {
+		n := rand.Intn(guess + 1)
+		if turn == 1 || n == guess {
+			fmt.Println("first shot you won ")
+			return
+		}
+		if n == guess {
+			fmt.Println("You won the game")
+			return
+		}
+	}
+	fmt.Println("sorry better luck next time ")
+
 }
